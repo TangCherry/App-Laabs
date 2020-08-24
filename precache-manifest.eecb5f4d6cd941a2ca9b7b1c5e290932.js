15b3bce70dfa0154bccd5ba664762b2967d74eac
TangCherry <danaee16@gmail.com>
2020-08-24 14:54:47 -0500

    Updates

---
+++ precache-manifest.eecb5f4d6cd941a2ca9b7b1c5e290932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6dd8f21da3cf6fa99f6d25f9d98b51",
+    "url": "/App-Laaps/index.html"
+  },
+  {
+    "revision": "95fcf9a4ad30cd339127",
+    "url": "/App-Laaps/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e8f6d49297771730fba8",
+    "url": "/App-Laaps/static/js/2.93031f80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/App-Laaps/static/js/2.93031f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fcf9a4ad30cd339127",
+    "url": "/App-Laaps/static/js/main.e67512b4.chunk.js"
+  },
+  {
+    "revision": "d112940c5485521732ac",
+    "url": "/App-Laaps/static/js/runtime-main.19f9a573.js"
+  }
+]);
